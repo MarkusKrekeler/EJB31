b539dd0b50dbb55c438b2aa137cc1f26150c67b1
krekeler <krekeler@LANURIO.adesso.local>
2013-02-01 17:21:11 +0100

    Weitere Änderungen für Deployment

--- airbusiness/src/main/java/de/adesso/airadessi/ejb/FlugServiceBean.java
+++ airbusiness/src/main/java/de/adesso/airadessi/ejb/FlugServiceBean.java
@@ -1,6 +1,39 @@
 package de.adesso.airadessi.ejb;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Stateful
 public class FlugServiceBean {
+
+	private final Logger logger = LoggerFactory
+			.getLogger(FlugServiceBean.class);
+
+	// Übung init Methode zum erstellen von Objekten
+	// Übung Injecten einer Stateless Session Bean zum konvertieren von Objekten
+
+	@PostConstruct
+	public void initBean() {
+		logger.info("Stateful Bean created!");
+	}
+
+	public void fluegeSuchen() {
+
+	}
+
+	public void flugBuchen() {
+
+	}
+
+	public void flugStornieren() {
+
+	}
+
+	public void flugUmbuchen() {
+
+	}
 
 }
